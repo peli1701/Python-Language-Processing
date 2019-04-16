996b66cded0e660a49a679d88a312e606b34fde7
Peter Lindee <33735133+peli1701@users.noreply.github.com>
2019-04-15 19:46:45 -0600

    Add files via upload

---
+++ basic_programs.py
@@ -0,0 +1,18 @@
+
+def compute_census(list_of_rates, current_population):
+    seconds = 31536000
+    birth_rate = seconds / 8
+    death_rate = seconds / 12
+    new_immigrants_rate = seconds / 33
+    future_pop = current_population + birth_rate + death_rate + new_immigrants_rate
+
+#def convert_seconds():
+
+
+#def generate_story(list_strings):
+
+
+
+def main():
+	print 'hello'
+   
